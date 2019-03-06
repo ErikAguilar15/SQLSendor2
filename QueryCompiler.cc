a5096e7b70051708079413b2bc12e55dd4736bac
alsendor <al_nucum@yahoo.com>
2019-03-05 16:15:47 -0800

    Currently implementing the query compiler

--- QueryCompiler.cc
+++ QueryCompiler.cc
@@ -10,6 +10,8 @@
 
 using namespace std;
 
+map <string, Scan> scanMap;
+map <string, Select> selectMap;
 
 QueryCompiler::QueryCompiler(Catalog& _catalog, QueryOptimizer& _optimizer) :
 	catalog(&_catalog), optimizer(&_optimizer) {
@@ -24,6 +26,29 @@ void QueryCompiler::Compile(TableList* _tables, NameList* _attsToSelect,
 	QueryExecutionTree& _queryTree) {
 
 	// create a SCAN operator for each table in the query
+	TableList * tables = _tables;
+	DBFile db
+	while (tables != NULL) {
+		Schema schema;
+		string tabName = tables->tableName;
+		catalog.GetSchema(tabName, schema);
+
+		//db.Open(&tab[0]); db.MoveFirst();
+
+		//Append scanned table to map of scanned tables
+		scanMap.insert (make_pair(tab,Scan(sch,db)));
+
+		CNF cnf;
+		Record rec;
+		cnf.ExtractCNF (*_predicate, sch, rec);
+
+		//Check Att list
+		if (cnf.numAnds > 0) {
+
+		}
+
+		tables = tables->next;
+	}
 
 	// push-down selections: create a SELECT operator wherever necessary
 
@@ -32,10 +57,13 @@ void QueryCompiler::Compile(TableList* _tables, NameList* _attsToSelect,
 	optimizer->Optimize(_tables, _predicate, root);
 
 	// create join operators based on the optimal order computed by the optimizer
+	RelationalOp* join = constTree(rootCopy, _predicate);
+	join->SetNoPages(pNum);
 
 	// create the remaining operators based on the query
 
 	// connect everything in the query execution tree and return
+	_queryTree.SetRoot(*join)
 
 	// free the memory occupied by the parse tree since it is not necessary anymore
 }
