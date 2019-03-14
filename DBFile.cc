8987f2a35fb02dd4a0c4854ee8476cc482d1983e
alsendor <al_nucum@yahoo.com>
2019-03-14 14:21:02 -0700

    yes

--- DBFile.cc
+++ DBFile.cc
@@ -30,58 +30,22 @@ DBFile& DBFile::operator=(const DBFile& _copyMe) {
 }
 
 int DBFile::Create (char* f_path, FileType f_type) {
-	ftype = f_type;
-	fileName = f_path;
-	return (file.Open(0,f_path));
 }
 
 int DBFile::Open (char* f_path) {
-	fileName = f_path;
-	return file.Open(1,f_path);
 }
 
 void DBFile::Load (Schema& schema, char* textFile) {
-	FILE * pFile;
-	string str = textFile;
-	pFile = fopen(&str[0],"r");
-
-	while (1)
-	{
-		Record rec;
-		if (rec.ExtractNextRecord (schema, *pFile) == 0) break;
-		AppendRecord(rec);
-	}
-
-	file.AddPage(page, file.GetLength());
-	fclose(pFile);
 }
 
 int DBFile::Close () {
-	if(ftype == Sorted || ftype == Index)
-		file.AddPage(page, file.GetLength());
-	return file.Close();
 }
 
 void DBFile::MoveFirst () {
-	pageNum = 0;
 }
 
 void DBFile::AppendRecord (Record& rec) {
-	if (page.Append(rec) == 0) {
-		file.AddPage(page, file.GetLength());
-		page.EmptyItOut();
-		page.Append(rec);
-		pageNum++;
-	}
 }
 
 int DBFile::GetNext (Record& rec) {
-	if (page.GetFirst(rec) == 0) {
-		if (file.GetLength() == pageNum) return 0;
-		if (file.GetPage(page, pageNum) == -1) return 0;
-		page.GetFirst(rec);
-		pageNum++;
-
-	}
-	return 1;
 }
